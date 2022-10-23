05c2d2c4feed1201aece723e2767bc2f0626b242
Marc Weber <marco-oweber@gmx.de>
2022-10-24 00:52:58 +0200

    dist

---
+++ dist/library-notes.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var notes = {
+    'dayjs': "mind utils-dayjs using unexpected failure",
+    'momentjs': 'use date-fns instead cause its maintained and smaller',
+    'knex-abstract': 'looks broken by design due to Knex.init() - how to have multiple connections ?'
+};
+exports.default = notes;
